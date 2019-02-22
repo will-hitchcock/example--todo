78bccd83302c4bc444dbaa8490c54b257be12a84
Will Hitchcock <willhitchcock@stockx.com>
2019-02-22 11:25:41 -0500

    remove unused styles

--- src/styles/margin.js
+++
@@ -1,3 +0,0 @@
-import { curry } from 'lodash/fp';
-
-export default curry((margin = 4, props) => ({ margin: `${margin}px` }));
--- src/styles/padding.js
+++
@@ -1,3 +0,0 @@
-import { curry } from 'lodash/fp';
-
-export default curry((unit, { padding }) => ({ padding: `${padding}${unit}` }));
